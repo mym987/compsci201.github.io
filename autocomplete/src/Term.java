8208af7dd7c319f1057e071b2c47c00266a19a0e
Mike Ma <ym67@duke.edu>
2015-10-30 22:30:28 -0400

    Update

--- autocomplete/src/Term.java
+++ autocomplete/src/Term.java
@@ -10,25 +10,31 @@ import java.util.Comparator;
 
 public class Term implements Comparable<Term> {
 
-	private final String word;
-	private final double weight;
+	private final String myWord;
+	private final double myWeight;
 
 	/**
-	 * The constructor for the Term class. Should set the values of word and 
+	 * The constructor for the Term class. Should set the values of word and
 	 * weight to the inputs, and throw the exceptions listed below
-	 * @param word The word this term consists of
-	 * @param weight The weight of this word in the Autocomplete algorithm
-	 * @throws NullPointerException if word is null
-	 * @throws IllegalArgumentException if weight is negative
+	 * 
+	 * @param word
+	 *            The word this term consists of
+	 * @param weight
+	 *            The weight of this word in the Autocomplete algorithm
+	 * @throws NullPointerException
+	 *             if word is null
+	 * @throws IllegalArgumentException
+	 *             if weight is negative
 	 */
 	public Term(String word, double weight) {
-		//TODO: Write this constructor
+		myWeight = weight;
+		myWord = word;
 	}
 
 	/**
-	 * A Comparator for comparing Terms using a set number of the letters
-	 * they start with. This Comparator may be useful in writing your
-	 * implementations of Autocompletors.
+	 * A Comparator for comparing Terms using a set number of the letters they
+	 * start with. This Comparator may be useful in writing your implementations
+	 * of Autocompletors.
 	 *
 	 */
 	public static class PrefixOrder implements Comparator<Term> {
@@ -38,43 +44,44 @@ public class Term implements Comparable<Term> {
 			this.r = r;
 		}
 
-		/**Compares v and w lexicographically using only their first r letters. 
+		/**
+		 * Compares v and w lexicographically using only their first r letters.
 		 * If the first r letters are the same, then v and w should be
-		 * considered equal. 
-		 * This method should take O(r) to run, and be independent
-		 * of the length of v and w's length.
-		 * You can access the Strings to compare using v.word and w.word.
-		 * @param v/w - Two Terms whose words are being compared
+		 * considered equal. This method should take O(r) to run, and be
+		 * independent of the length of v and w's length. You can access the
+		 * Strings to compare using v.word and w.word.
+		 * 
+		 * @param v/w
+		 *            - Two Terms whose words are being compared
 		 */
 		public int compare(Term v, Term w) {
-			//TODO: Implement this method
+			String vS = v.myWord.substring(0, Math.min(r, v.myWord.length()));
+			String wS = w.myWord.substring(0, Math.min(r, w.myWord.length()));
+			return vS.compareTo(wS);
 		}
 	}
 
 	/**
 	 * A Comparator for comparing Terms using only their weights, in descending
-	 * order.
-	 * This Comparator may be useful in writing your
-	 * implementations of Autocompletor
+	 * order. This Comparator may be useful in writing your implementations of
+	 * Autocompletor
 	 *
 	 */
 	public static class ReverseWeightOrder implements Comparator<Term> {
 		public int compare(Term v, Term w) {
-			//TODO: Implement this method			
+			return Double.compare(w.myWeight, v.myWeight);
 		}
 	}
 
-
 	/**
 	 * A Comparator for comparing Terms using only their weights, in ascending
-	 * order.
-	 * This Comparator may be useful in writing your
-	 * implementations of Autocompletor
+	 * order. This Comparator may be useful in writing your implementations of
+	 * Autocompletor
 	 *
 	 */
 	public static class WeightOrder implements Comparator<Term> {
 		public int compare(Term v, Term w) {
-			//TODO: Implement this method			
+			return Double.compare(v.myWeight, w.myWeight);
 		}
 	}
 
@@ -82,21 +89,21 @@ public class Term implements Comparable<Term> {
 	 * The default sorting of Terms is lexicographical ordering.
 	 */
 	public int compareTo(Term that) {
-		return word.compareTo(that.word);
+		return myWord.compareTo(that.myWord);
 	}
 
 	/**
 	 * Getter methods, use these in other classes which use Term
 	 */
 	public String getWord() {
-		return word;
+		return myWord;
 	}
 
 	public double getWeight() {
-		return weight;
+		return myWeight;
 	}
 
 	public String toString() {
-		return String.format("%14.1f\t%s", weight, word);
+		return String.format("%14.1f\t%s", myWeight, myWord);
 	}
 }
--- autocomplete/src/TrieAutocomplete.java
+++ autocomplete/src/TrieAutocomplete.java
@@ -1,3 +1,7 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * General trie/priority queue algorithm for implementing Autocompletor
  * 
@@ -12,13 +16,16 @@ public class TrieAutocomplete implements Autocompletor {
 	protected Node myRoot;
 
 	/**
-	 * Constructor method for TrieAutocomplete. Should initialize
-	 * the trie rooted at myRoot, as well as add all nodes necessary to 
-	 * represent the words in terms.
-	 * @param terms - The words we will autocomplete from
-	 * @param weights - Their weights, such that terms[i] has weight
-	 * weights[i].
-	 * @throws a NullPointerException if either argument is null
+	 * Constructor method for TrieAutocomplete. Should initialize the trie
+	 * rooted at myRoot, as well as add all nodes necessary to represent the
+	 * words in terms.
+	 * 
+	 * @param terms
+	 *            - The words we will autocomplete from
+	 * @param weights
+	 *            - Their weights, such that terms[i] has weight weights[i].
+	 * @throws a
+	 *             NullPointerException if either argument is null
 	 */
 	public TrieAutocomplete(String[] terms, double[] weights) {
 		if (terms == null || weights == null)
@@ -32,62 +39,125 @@ public class TrieAutocomplete implements Autocompletor {
 	}
 
 	/**
-	 * Add the word with given weight to the trie. 
-	 * If word already exists in the trie, no new nodes should be created, 
-	 * but the weight of word should be updated.
+	 * Add the word with given weight to the trie. If word already exists in the
+	 * trie, no new nodes should be created, but the weight of word should be
+	 * updated.
 	 * 
-	 * In adding a word, this method should do the following:
-	 * 	Create any necessary intermediate nodes if they do not exist.
-	 *  Update the subtreeMaxWeight of all nodes in the path from root to
-	 *  the node representing word.
-	 *  Set the value of myWord, myWeight, isWord, and mySubtreeMaxWeight of 
-	 *  the node corresponding to the added word to the correct values
-	 *  
-	 * @throws a NullPointerException if word is null
-	 * @throws an IllegalArgumentException if weight is negative.
+	 * In adding a word, this method should do the following: Create any
+	 * necessary intermediate nodes if they do not exist. Update the
+	 * subtreeMaxWeight of all nodes in the path from root to the node
+	 * representing word. Set the value of myWord, myWeight, isWord, and
+	 * mySubtreeMaxWeight of the node corresponding to the added word to the
+	 * correct values
+	 * 
+	 * @throws a
+	 *             NullPointerException if word is null
+	 * @throws an
+	 *             IllegalArgumentException if weight is negative.
 	 * 
 	 */
 	private void add(String word, double weight) {
-		//TODO: Implement this method
+		Node p = myRoot;
+		p.mySubtreeMaxWeight = Math.max(p.mySubtreeMaxWeight, weight);
+		for (int i = 0; i < word.length(); i++) {
+			char c = word.charAt(i);
+			if (!p.children.containsKey(c))
+				p.children.put(c, new Node(c, p, weight));
+			p = p.getChild(c);
+			p.mySubtreeMaxWeight = Math.max(p.mySubtreeMaxWeight, weight);
+		}
+		p.isWord = true;
+		p.myWord = word;
+		p.myWeight = weight;
 	}
 
 	@Override
 	/**
-	 * Required by the Autocompletor interface.
-	 * Returns an array containing the k words in the trie with the largest weight
-	 * which match the given prefix, in descending weight order. If less than k
-	 * words exist matching the given prefix (including if no words exist),
-	 * then the array instead contains all those words.
-	 * e.g. If terms is {air:3, bat:2, bell:4, boy:1}, then topKMatches("b", 2)
-	 * should return {"bell", "bat"}, but topKMatches("a", 2) should return
-	 * {"air"}
+	 * Required by the Autocompletor interface. Returns an array containing the
+	 * k words in the trie with the largest weight which match the given prefix,
+	 * in descending weight order. If less than k words exist matching the given
+	 * prefix (including if no words exist), then the array instead contains all
+	 * those words. e.g. If terms is {air:3, bat:2, bell:4, boy:1}, then
+	 * topKMatches("b", 2) should return {"bell", "bat"}, but topKMatches("a",
+	 * 2) should return {"air"}
 	 * 
-	 * @param prefix - A prefix which all returned words must start with
-	 * @param k - The (maximum) number of words to be returned
-	 * @return An array of the k words with the largest weights among all
-	 * words starting with prefix, in descending weight order.
-	 * 	If less than k such words exist, return an array containing all those 
-	 *  words
-	 * 	If no such words exist, return an empty array
-	 * @throws a NullPointerException if prefix is null
+	 * @param prefix
+	 *            - A prefix which all returned words must start with
+	 * @param k
+	 *            - The (maximum) number of words to be returned
+	 * @return An array of the k words with the largest weights among all words
+	 *         starting with prefix, in descending weight order. If less than k
+	 *         such words exist, return an array containing all those words If
+	 *         no such words exist, return an empty array
+	 * @throws a
+	 *             NullPointerException if prefix is null
 	 */
 	public String[] topKMatches(String prefix, int k) {
-		//TODO: Implement this method
+		Node p = find(myRoot, prefix);
+		if (p == null)
+			return new String[0];
+		List<Node> list = new LinkedList<>();
+		traverse(p, list);
+		return list.stream()
+				.sorted(Collections.reverseOrder())
+				.map(n -> n.myWord)
+				.distinct().limit(k)
+				.toArray(n -> new String[n]);
+	}
+
+	private Node find(Node p, String prefix) {
+		for (int i = 0; i < prefix.length(); i++) {
+			char c = prefix.charAt(i);
+			if (p.getChild(c) == null)
+				return null;
+			p = p.getChild(c);
+		}
+		return p;
+	}
+
+	private void traverse(Node root, List<Node> list) {
+		if (root.isWord)
+			list.add(root);
+		root.children.forEach((k, v) -> traverse(v, list));
 	}
 
 	@Override
 	/**
-	 * Given a prefix, returns the largest-weight word in the trie starting with 
-	 * that prefix. 
+	 * Given a prefix, returns the largest-weight word in the trie starting with
+	 * that prefix.
 	 * 
-	 * @param prefix - the prefix the returned word should start with
-	 * @return The word from _terms with the largest weight starting with 
-	 * prefix, or an empty string if none exists
-	 * @throws a NullPointerException if the prefix is null
+	 * @param prefix
+	 *            - the prefix the returned word should start with
+	 * @return The word from _terms with the largest weight starting with
+	 *         prefix, or an empty string if none exists
+	 * @throws a
+	 *             NullPointerException if the prefix is null
 	 * 
 	 */
 	public String topMatch(String prefix) {
-		//TODO: Implement this method
+		Node p = find(myRoot, prefix);
+		return p == null ? "" : find(p, p.mySubtreeMaxWeight).myWord;
+	}
+
+	private Node find(Node p, double weight) {
+		if(p.isWord && p.myWeight == weight)
+			return p;
+		for(char ch:p.children.keySet()){
+			Node t = find(p.getChild(ch),weight);
+			if(t!=null)
+				return t;
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		String[] a = {"a","ab","abc","abcd","abcde"};
+		double[] b = {1,2,3,5,5};
+		TrieAutocomplete tr = new TrieAutocomplete(a,b);
+		//for (String s : tr.topKMatches("ab", 5))
+			//System.out.println(s);
+		System.out.println(tr.topMatch(""));
+		//System.out.println(lastIndexOf(t, new Term("dbcd", 0), new Term.PrefixOrder(6)));
 	}
 
 }
